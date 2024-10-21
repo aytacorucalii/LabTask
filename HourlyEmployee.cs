649d0e5b0abaeddb7421cc0754b19d74faea3455
Aytacorucali <aytacho-ap215@code.edu.az>
2024-10-22 00:01:44 +0400

    Employee Abstract Lab TAskk

---
+++ HourlyEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp9.NewFolder
+{
+    internal class HourlyEmployee : EmployeAbstract
+    {
+        public double HourlyRate { get; set; }
+        public double HourlyWorked { get; set; }
+
+        public override double CalculateSalary() => HourlyRate * HourlyWorked;
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Ad: {Name}");
+            Console.WriteLine($"Soyad: {Surname}");
+            Console.WriteLine($"Yaş: {Age}");
+            Console.WriteLine($"Saatlıq Ücret: {HourlyRate}");
+            Console.WriteLine($"İşlənən Saat: {HourlyWorked}");
+        }
+    }
+}
