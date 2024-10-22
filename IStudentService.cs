5b9b3df88e28fccc7c000084bfce788b974e5f6d
Aytacorucali <aytacho-ap215@code.edu.az>
2024-10-23 00:46:22 +0400

    Student Service LabTAsk

---
+++ IStudentService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentServiceeTaskk.Models
+{
+    internal interface IStudentService
+    {
+        Student GetStudent(int id);
+        Student[] GetAllStudents();
+        Student[] GetStudentByName();
+         void AddStudent(Student student);    
+         void UpdateStudent(int id);
+         void removeStudent(int id, bool isSoftDelete);
+     
+        Student GetStudentByName(string Name);
+    }
+}
---
+++ StudentServiceProgram.cs
@@ -0,0 +1,72 @@
+﻿using StudentServiceeTaskk.Models;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+     
+        StudentService studentService = new StudentService();
+
+     
+        Student aytac = new Student();
+        aytac.FirstName = "Aytac";
+        aytac.LastName = "Oruc";
+        aytac.GPA = 3.5; 
+        aytac.Status = "Active"; 
+        aytac.Major = "Computer Science"; 
+        studentService.AddStudent(aytac);
+
+
+        Student elvin = new Student();
+        elvin.FirstName = "Elvin";
+        elvin.LastName = "Huseynov";
+        elvin.GPA = 3.8;
+        elvin.Status = "Graduate"; 
+        elvin.Major = "Math";
+        studentService.AddStudent(elvin);
+
+        Student nigar = new Student();
+        nigar.FirstName = "Nigar";
+        nigar.LastName = "Aliyeva";
+        nigar.GPA = 3.6;
+        nigar.Status = "Pending"; 
+        nigar.Major = "Physics";
+        studentService.AddStudent(nigar);
+
+        Student kamran = new Student();
+        kamran.FirstName = "Kamran";
+        kamran.LastName = "Mehdiyev";
+        kamran.GPA = 3.7;
+        kamran.Status = "Active";
+        kamran.Major = "Chemistry";
+        studentService.AddStudent(kamran);
+
+        Student[] allStudents = studentService.GetAllStudents();
+
+       
+        foreach (Student student in allStudents)
+        {
+            if (student != null) 
+            {
+               
+                Console.WriteLine($"Id: {student.Id}, Ad: {student.FirstName}, Soyad: {student.LastName}, Ixtisas: {student.Major}, GPA: {student.GPA}, Status: {student.Status}");
+            }
+        }
+
+
+
+
+
+        Student foundStudent = studentService.GetStudentById(2);
+
+        if (foundStudent != null)
+        {
+           
+            Console.WriteLine($"Tapılan tələbə: Id: {foundStudent.Id}, Ad: {foundStudent.FirstName}, Soyad: {foundStudent.LastName}, Major: {foundStudent.Major}");
+        }
+        else
+        {
+            Console.WriteLine("Tələbə tapılmadı.");
+        }
+    }
+}
