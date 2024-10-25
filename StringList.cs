565e10ceb9872b545986da4694a6437239b9df10
Aytacorucali <aytacho-ap215@code.edu.az>
2024-10-25 09:11:26 +0400

    Indexer Task

---
+++ StringList.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+
+namespace IndexerTask.Models
+{
+    internal class StringList
+    {
+        private string[] Cities;
+
+        public StringList(string[] initCity)
+        {
+            Cities = initCity;
+        }
+
+
+        public int this[string cityName]
+        {
+            get
+            {
+                for (int i = 0; i < Cities.Length; i++)
+                {
+                    if (Cities[i] == cityName)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
