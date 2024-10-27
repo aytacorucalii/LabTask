5ca8ab272ff395ec73265cbee425ae1ea5b41c4a
Aytacorucali <aytacho-ap215@code.edu.az>
2024-10-27 22:06:20 +0400

    Custom Validator add

---
+++ CustomValidatorLibrary/CustomValidatorLibrary/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using CustomValidatorLibrary;
+
+namespace ExecutableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validator validator = new Validator();
+            Person person = new Person();
+
+            while (true) 
+            {
+                Console.WriteLine("1. Yeni şəxs yarat");
+                Console.WriteLine("2. Çıxış");
+                Console.Write("Seçiminizi daxil edin: ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    
+                    Console.Write("Username: ");
+                    string username = Console.ReadLine();
+                    if (!validator.ValidateUserName(username))
+                    {
+                        Console.WriteLine("İstifadəçi adı minimum 2 simvol olmalıdır.");
+                        continue; 
+                    }
+                    person.UserName = username;
+
+                    Console.Write("Password: ");
+                    string password = Console.ReadLine();
+                    if (!validator.ValidatePassword(password))
+                    {
+                       Console.WriteLine("Şifrə minimum 8 simvol olmalı, rəqəm, simvol, böyük və kiçik hərf r.");
+                       continue;
+                    }
+                    person.Password = password;
+
+                    Console.Write("Name: ");
+                    person.Name = Console.ReadLine();
+                    if (!validator.ValidateName(person.Name))
+                    {
+                        Console.WriteLine("Ad minimum 2 simvol olmalıdır.");
+                        continue; 
+                    }
+
+                    Console.Write("Surname: ");
+                    person.Surname = Console.ReadLine();
+                    if (!validator.ValidateSurname(person.Surname))
+                    {
+                        Console.WriteLine("Soyad minimum 2 simvol olmalıdır.");
+                        continue; 
+                    }
+
+                    Console.Write("Age: ");
+                    person.Age = int.Parse(Console.ReadLine());
+                    if (!validator.ValidateAge(person.Age))
+                    {
+                        Console.WriteLine("Yaş 0-dan böyük, 55d olmalıdır.");
+                        continue; 
+                    }
+
+                    Console.Write("Birthday (yyyy-mm-dd): ");
+                    person.Birthday = DateTime.Parse(Console.ReadLine());
+                    if (!validator.ValidateBirthday(person.Birthday))
+                    {
+                        Console.WriteLine("Doğum tarixi 1970-ci ildən kiçik olmamalıdır.");
+                        continue; 
+                    }
+
+                    Console.WriteLine("Yeni şəxs yaradıldı!");
+                }
+                else if (choice == "2")
+                {
+                    Console.WriteLine("Proqramdan çıxılır...");
+                    break; 
+                }
+                else
+                {
+                    Console.WriteLine("Yanlış seçim! Lütfən, 1 və ya 2 arasında seçim edin.");
+                }
+            }
+        }
+    }
+}
---
+++ CustomValidatorLibrary/CustomValidatorLibrary/Validator/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ExecutableProject
+{
+    public class Person
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
+
